890b27cea5678a0c86ece56d8f1e802ef961c477
Kyle Hoff <kylehoff01@gmail.com>
2016-01-16 12:43:58 -0700

    Added test cases for ModelUnit.java and added toString() method for testing.

--- StatTrack/src/ModelUnit.java
+++ StatTrack/src/ModelUnit.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * @author kylehoff
@@ -10,6 +11,9 @@ public class ModelUnit {
 	public String destroyedMessage = "The model is destroyed and cannot make attacks";
 	public ArrayList<Attack> attacks = new ArrayList<Attack>();
 	public boolean destroyed = false;
+	public int meleeAttacks = 0;
+	public int rangedAttacks = 0;
+	public int magicAttacks = 0;
 	
 	
 	// CONSTRUCTORS
@@ -49,6 +53,7 @@ public class ModelUnit {
 		
 		attacks.add(new Attack(AttackType.MELEE,BHit, HDice, HRoll, H,
 				BDmg, DDice, DRoll, DDealt));
+		meleeAttacks++;
 	}
 	
 	/**
@@ -64,6 +69,7 @@ public class ModelUnit {
 		
 		attacks.add(new Attack(AttackType.RANGED,BHit, HDice, HRoll, H,
 				BDmg, DDice, DRoll, DDealt));
+		rangedAttacks++;
 	}
 	
 	/**
@@ -79,5 +85,60 @@ public class ModelUnit {
 		
 		attacks.add(new Attack(AttackType.MAGIC,BHit, HDice, HRoll, H,
 				BDmg, DDice, DRoll, DDealt));
+		magicAttacks++;
+	}
+	
+	@Override
+	public String toString() {
+		String str = String.format("%-15s\n %-18s %-18s %-18s\n %-18d %-18d %-18d",
+				name, "Melee Attacks", "Ranged Attacks", "Magic Attacks",
+				meleeAttacks, rangedAttacks, magicAttacks);
+		return str;
+	}
+
+	public static void main(String[] args){
+		
+		ModelUnit caster = new ModelUnit();
+//		System.out.println(caster.name);
+		System.out.println(caster);
+		
+		Random rand = new Random();
+		
+		for(int i = 0; i < 25; i++){
+			boolean boostHit = rand.nextBoolean();
+			int hitDice = rand.nextInt(1) + 2;
+			if(boostHit)
+				hitDice++;
+			int hitRoll = rand.nextInt(hitDice * 6 - hitDice) + hitDice;
+			boolean hit = rand.nextBoolean();
+			boolean boostDmg = false;
+			int dmgDice = 0;
+			int dmgRoll = 0;
+			int dmgDealt = 0;
+			if(hit){
+				boostDmg = rand.nextBoolean();
+				dmgDice = rand.nextInt(2) + 2;
+				if(boostDmg)
+					dmgDice++;
+				dmgRoll = rand.nextInt(dmgDice * 6 - dmgDice) + dmgDice + 1;
+				dmgDealt = rand.nextInt(dmgDice * 6 - dmgDice) + dmgDice;
+			}
+			AttackType t = AttackType.getRandom();
+			if(t.equals(AttackType.MELEE))
+				caster.meleeAttack(boostHit, hitDice, hitRoll,
+						hit, boostDmg, dmgDice, dmgRoll, dmgDealt);
+			else if(t.equals(AttackType.RANGED))
+				caster.rangedAttack(boostHit, hitDice, hitRoll,
+						hit, boostDmg, dmgDice, dmgRoll, dmgDealt);
+			else if(t.equals(AttackType.MAGIC))
+				caster.magicAttack(boostHit, hitDice, hitRoll,
+						hit, boostDmg, dmgDice, dmgRoll, dmgDealt);
+		}
+//		System.out.println(caster.meleeAttacks);
+//		System.out.println(caster.rangedAttacks);
+//		System.out.println(caster.magicAttacks);
+		System.out.println(caster);
+		caster.destroyed();
+		caster.magicAttack(false, 2, 7, true, false, 2, 12, 16);
 	}
 }
